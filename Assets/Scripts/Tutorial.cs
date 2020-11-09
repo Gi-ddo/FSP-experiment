6bda843263e788f8c0037282951aa125aceb4120
Gi-ddo <60913804+Gi-ddo@users.noreply.github.com>
2020-11-10 01:55:25 +0200

    Revert "Revert "Iteration 2 submission""
    This reverts commit 80fd6d319b02eec7bef418ce9c228dd934356f9e.

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Tutorial : MonoBehaviour
+{   
+    [Header("Intro room text")]
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+
+    [Header("Practice room text")]
+    public GameObject room_intro;
+    public GameObject weapon_hint;
+    public GameObject room_objective;
+    public GameObject bounciness_section;
+
+
+     void Start()
+    {
+        Invoke("activate_intro_txt", 15f);
+    }
+
+    
+    void activate_intro_txt()
+    {
+        text1.SetActive(false);
+        text2.SetActive(false);
+        text3.SetActive(false);
+    }
+
+    public void activate_pract_txt()
+    {
+        room_intro.SetActive(true);
+        weapon_hint.SetActive(true);
+        room_objective.SetActive(true);
+        bounciness_section.SetActive(true);
+    }
+
+    public void disable_prac_txt()
+    {
+        room_intro.SetActive(false);
+        weapon_hint.SetActive(false);
+        room_objective.SetActive(false);
+
+    }
+}
